aa9f7cd56007a0b0696d933d999bcfa9ac01033e
alexandr_basso <alexandr.basso@gmail.com>
2023-09-08 15:35:17 +0300

    updated entire website, old one is in /en/index

--- main_page/mypage/urls.py
+++ main_page/mypage/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from mypage.views import IndexView
-
+from mypage.views import IndexView, CVView, Index2View
 
 app_name = "mypage"
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="index")
+    path("", Index2View.as_view(), name="index2"),
+    path("index", IndexView.as_view(), name="index"),
+    path("cv", CVView.as_view(), name="cv"),
+    path("cv2", CVView.as_view(), name="cv2"),
 ]
--- main_page/mypage/views.py
+++ main_page/mypage/views.py
@@ -6,7 +6,6 @@ from rest_framework.views import APIView
 from django.utils.translation import gettext_lazy as _
 
 
-
 class IndexView(APIView):
     renderer_classes = [TemplateHTMLRenderer]
     template_name = "mypage/index.html"
@@ -19,7 +18,8 @@ class IndexView(APIView):
         data["data1"] = 'Some text'
         programing_skills = [["Python"], ["Django", "DRF"], ["HTML", "CSS", "Bootstrap", "Ajax", "Postman"],
                              ["Flask", "API", "Docker", "Swagger", "Aiogram"], ["SQLite", "MySQL", "PostgreSQL"],
-                             ["Git","GitHub", "GitLab"], ["NumPy", "Pandas", "Agile", "Odoo"], ["OOP", "DRY", "SOLID", "CRUD", "KISS"]]
+                             ["Git", "GitHub", "GitLab"], ["NumPy", "Pandas", "Agile", "Odoo"],
+                             ["OOP", "DRY", "SOLID", "CRUD", "KISS"]]
         # programing_skills = "Python, Django, DRF, Git, API, SQLite, MySQL, PostgreSQL, Flask, HTML, CSS, Bootstrap, Aiogram, Docker, Swagger, OOP, DRY, SOLID CRUD KISS principles, Ajax, NumPy, Pandas, Agile, Postman, GitHub, GitLab, Odoo framework "
         data["programing_skills"] = programing_skills
         other_skills = ["Team player", "Project Management", "Time management", "Administration", "Accounting"]
@@ -32,5 +32,56 @@ class IndexView(APIView):
         data["project_two"] = project_two
         project_three = ["Python", "Aiogram", "API", "SQLite", "Docker"]
         data["project_three"] = project_three
+
+        return Response(data)
+
+
+class Index2View(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "mypage/index2.html"
+
+    def get(self, request: Request) -> Response:
+        # print('printing request: ', request)
+        # print('printing request.LANGUAGE_CODE : ', request.LANGUAGE_CODE)
+        # print('printing request.data: ', request.data)
+        data = request.data
+        data["data1"] = 'Some text'
+        programing_skills = [["Python", "Django", "DRF", "Flask", "Git", "API"],
+                             ["HTML", "CSS", "Bootstrap", "Ajax", "Postman"],
+                             ["SQL", "PostgreSQL", "NumPy", "Pandas", "Agile", "Odoo"],
+                             ["Docker", "Swagger", "Aiogram"],
+                             ["OOP", "DRY", "SOLID", "CRUD", "KISS"]]
+        # programing_skills = "Python, Django, DRF, Git, API, SQLite, MySQL, PostgreSQL, Flask, HTML, CSS, Bootstrap, Aiogram, Docker, Swagger, OOP, DRY, SOLID CRUD KISS principles, Ajax, NumPy, Pandas, Agile, Postman, GitHub, GitLab, Odoo framework "
+        data["programing_skills"] = programing_skills
+        other_skills = ["Team player", "Project Management", "Time management", "Administration", "Accounting"]
+        data["other_skills"] = other_skills
+        language_skills = ["English", "Russian", "Romanian", "Italian"]
+        data["language_skills"] = language_skills
+        project_one = ["Python", "Django", "DRF", "API", "SQLite", "HTML", "CSS", "Bootstrap", "Docker", "Swagger"]
+        data["project_one"] = project_one
+        project_two = ["Python", "Django", "DRF", "API", "SQLite", "HTML", "Bootstrap", "Docker"]
+        data["project_two"] = project_two
+        project_three = ["Python", "Aiogram", "API", "SQLite", "Docker"]
+        data["project_three"] = project_three
+
+        return Response(data)
+
+
+class CVView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "mypage/cv.html"
+
+    def get(self, request: Request) -> Response:
+        data = request.data
+
+        return Response(data)
+
+
+class CVView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "mypage/cv2.html"
+
+    def get(self, request: Request) -> Response:
+        data = request.data
 
         return Response(data)
